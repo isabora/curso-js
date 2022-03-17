1d87fa9070225c802062ef64d8b4c2cf52fae45d
Isabeli Bora <isabelibora@hotmail.com>
2022-03-16 22:21:35 -0300

    aula4-6

---
+++ script.js
@@ -0,0 +1,20 @@
+// let nome = isabeli;
+// let idade = 20;
+// let altura = 1.56;
+
+// console.log (nome);
+// console.log (idade);
+// console.log (altura);
+
+
+// FUNÇÃO: Verbo + Substantivo
+// EXEMPLOS: transformarObjeto / alterarTamanho
+let corSite = "azul";
+function resetaCor(cor){
+    corSite = "cor";
+};
+
+// console.log(corSite);
+// resetaCor();
+// console.log(corSite);
+
